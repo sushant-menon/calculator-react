ebbf7882c8c60af3aa39e938a37345b9123cd833
Sushant <sushantnambiar@gmail.com>
2023-03-23 23:59:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
@@ -56,6 +63,27 @@ function reducer(state, { type, payload }) {
     case ACTIONS.CLEAR:
       return {};
 
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
@@ -67,6 +95,7 @@ function reducer(state, { type, payload }) {
 
       return {
         ...state,
+        overwrite: true,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
